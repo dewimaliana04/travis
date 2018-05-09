47c40721e077f5c4bd915b2e05b1665dbf697b6e
Dewi Maliana <dewimaliana04@gmail.com>
2018-05-09 16:34:30 +0700

    praktek2

--- stok-app-master/koneksi.php
+++ stok-app-master/koneksi.php
@@ -2,8 +2,8 @@
 class Koneksi {
     private $server = "localhost";
     private $username = "dev"; // root
-    private $password = "rahasia"; // ""
-    private $db = "phb_kelas_c";
+    private $password = "dewimaliana"; // ""
+    private $db = "id4927486_stok_barang";
 
     function getKoneksi() {
         return new mysqli($this->server, $this->username, 
